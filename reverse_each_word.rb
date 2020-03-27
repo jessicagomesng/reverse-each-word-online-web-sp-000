c5c6efb0189425c2d21d788b4c676fcc7a881b4e
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 16:40:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = []
-  array.each do |word|
-  new_array << word.reverse
+  array.collect do |word|
+  word.reverse
   end
-  new_array.join(" ")
+  array.join(" ")
 end
 
 def reverse_each_wordv2(string)
